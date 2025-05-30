15e4e904e747539713a3f90d9f3ea71c8d0f076c
MSI\icewi <icewindflyhero@gmail.com>
2025-05-30 21:38:57 +0800

    加入專案檔案。

---
+++ Pi Calculator/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pi_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<int, result> results = new Dictionary<int, result>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataTable.ItemsSource = new List<result>();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(number.Text, out int sampleSize)) 
+            {
+                if (!results.ContainsKey(sampleSize))
+                {
+                    var stopWatech = Stopwatch.StartNew();
+                    double pi = Calculator.calculate(sampleSize);
+                    stopWatech.Stop();
+
+                    var data = new result
+                    {
+                        sample = sampleSize,
+                        time = stopWatech.Elapsed,
+                        value = pi
+                    };
+
+                    results[sampleSize] = data;
+
+                    dataTable.ItemsSource = null;
+                    dataTable.ItemsSource = results.Values.ToList();
+                }
+
+                else if (results.ContainsKey(sampleSize))
+                {
+                    MessageBox.Show("資料已經存在");
+                }
+
+                else 
+                {
+                    MessageBox.Show("請輸入有效數值");
+                }
+            
+            
+            }
+        }
+    }
+}
---
+++ Pi Calculator/result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi_Calculator
+{
+    public class result
+    {
+        public int sample {  get; set; }
+        public TimeSpan time { get; set; }
+        public double value { get; set; }
+    }
+}
